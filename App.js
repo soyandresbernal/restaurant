89c00806eba8257c044605c7167ffbcc9648482c
SeriousAndres <soyandresbernal@gmail.com>
2022-08-31 12:59:45 -0500

    Make:Navigation app

--- App.js
+++ App.js
@@ -1,20 +1,4 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-
+import Navigation from "./navigations/Navigation";
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>new message!</Text>
-      <StatusBar style='auto' />
-    </View>
-  );
+  return <Navigation />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
