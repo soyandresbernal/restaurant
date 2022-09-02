d2b1677e10e86620f98758efe19f93a331ed0fcb
SeriousAndres <soyandresbernal@gmail.com>
2022-09-02 12:48:11 -0500

    fix:nav

---
+++ utils/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyABZ1ZQ6X0OWzbHZPbaL0ix3tVtrWnOdW0",
+  authDomain: "restaurants-1fccc.firebaseapp.com",
+  projectId: "restaurants-1fccc",
+  storageBucket: "restaurants-1fccc.appspot.com",
+  messagingSenderId: "451985952555",
+  appId: "1:451985952555:web:fdcc6742f8a51b7836bf6e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
